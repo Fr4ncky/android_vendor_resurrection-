RR-Security/android_vendor_resurrection

add some prebuilt stuff

config/common.mk
@@ -76,6 +76,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/Adaway/Adaway.apk:system/app/Adaway/Adaway.apk
 
+# Weather Provider
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/OpenWeatherMapWeatherProvider/OpenWeatherMapWeatherProvider.apk:system/app/OpenWeatherMapWeatherProvider/OpenWeatherMapWeatherProvider.apk \
+    vendor/cm/prebuilt/WundergroundWeatherProvider/WundergroundWeatherProvider.apk:system/app/WundergroundWeatherProvider/WundergroundWeatherProvider.apk \
+    vendor/cm/prebuilt/YahooWeatherProvider/YahooWeatherProvider.apk:system/app/YahooWeatherProvider/YahooWeatherProvider.apk
+
 # KernelAdiutor
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/KernelAdiutor/KernelAdiutor.apk:system/app/KernelAdiutor/KernelAdiutor.apk

Fr4ncky/android_vendor_resurrection-

Conflicts:
	config/common.mk
	prebuilt/common/magisk.zip

config/common.mk
@@ -73,8 +73,8 @@ PRODUCT_COPY_FILES += \
     vendor/cm/Features.mkdn:system/etc/RR/Features.txt
     
 # Adaway
-#PRODUCT_COPY_FILES += \
-#    vendor/cm/prebuilt/Adaway/Adaway.apk:system/app/Adaway/Adaway.apk
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/Adaway/Adaway.apk:system/app/Adaway/Adaway.apk
 
 # Weather Provider
 PRODUCT_COPY_FILES += \